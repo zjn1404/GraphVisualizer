4ea069a51983fa752b102ccfa4c444409440d3dc
zjn1404 <nqt460@gmail.com>
2024-03-16 10:35:01 +0700

    Delete src/visualizer directory

--- src/visualizer/src/visualizer/ApplicationRunner.java
+++
@@ -1,8 +0,0 @@
-package visualizer;
-
-public class ApplicationRunner {
-    public static void main(String[] args) {
-        MainFrame frame = new MainFrame();
-        frame.setVisible(true);
-    }
-}
